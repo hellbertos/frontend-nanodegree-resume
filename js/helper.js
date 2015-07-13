f99527c0c89269ecb93298bc0a8ebc705f456c65
James R <james.ruggieri@yahoo.com>
2015-07-12 18:10:44 -0700

    Add structural div and classes to School section

--- js/helper.js
+++ js/helper.js
@@ -44,10 +44,10 @@ var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '&nbsp;-- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%&nbsp;</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<div class="emphasis dark-grey-txt"><br>Major: %data%</div>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#"> %data%';
